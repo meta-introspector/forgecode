27db131022d7341c8261a4a3f9e21eb2a2ebb95c
mike dupont <gemini-agent-47@solfunmeme.com>
2026-06-22 18:09:31 -0400

    builds

--- flake.nix
+++ flake.nix
@@ -72,11 +72,25 @@
               pkgs.perl
               pkgs.pkg-config
               pkgs.protobuf
+            ]
+            ++ lib.optionals pkgs.stdenv.isLinux [
+              pkgs.openssl.dev
+              pkgs.libxkbcommon
+              pkgs.libx11
+              pkgs.libxext
+              pkgs.libxfixes
+              pkgs.libxcb
+              pkgs.wayland
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.libiconv
+              pkgs.apple-sdk
             ];
 
             buildInputs =
               [ pkgs.sqlite ]
               ++ lib.optionals pkgs.stdenv.isLinux [
+                pkgs.openssl.dev
                 pkgs.libxkbcommon
                 pkgs.libx11
                 pkgs.libxext
@@ -143,8 +157,15 @@
                 pkgs.protobuf
                 rustToolchain
                 pkgs.sqlite
+                # System libraries needed for libgit2-sys and curl-sys
+                pkgs.libgit2
+                pkgs.curl
+                pkgs.libssh2
+                pkgs.zlib
+                pkgs.nghttp2
               ]
               ++ lib.optionals pkgs.stdenv.isLinux [
+                pkgs.openssl.dev
                 pkgs.libxkbcommon
                 pkgs.libx11
                 pkgs.libxext
@@ -162,6 +183,15 @@
             PROTOC = "${pkgs.protobuf}/bin/protoc";
             PROTOC_INCLUDE = "${pkgs.protobuf}/include";
             APP_VERSION = "0.1.0-dev";
+
+            shellHook = ''
+              export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+              export OPENSSL_LIB_DIR="${pkgs.openssl.dev}/lib";
+              export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include";
+              export OPENSSL_STATIC=1;
+              export LD_LIBRARY_PATH="${pkgs.openssl.dev}/lib";
+              export LD_RUN_PATH="${pkgs.openssl.dev}/lib";
+            '';
           };
         });
     };
